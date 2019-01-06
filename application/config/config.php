cd4af24a71df24b598b28c9974b0eaec1a2c934d
rrmanoj <rr.manoj@gmail.com>
2019-01-06 10:15:15 +0000

    addes comment for testing

--- application/config/config.php
+++ application/config/config.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
+added comment from manoj for testing
 |--------------------------------------------------------------------------
 | Base Site URL
 |--------------------------------------------------------------------------
